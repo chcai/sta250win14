12a03dd3209c4a73261cadb539c46c95c2a5b212
chcai <chcai@ucdavis.edu>
2014-01-17 20:21:08 -0800

    2nd method: gets results

--- sta250win14hw1b/sta250win14hw1b.R
+++ sta250win14hw1b/sta250win14hw1b.R
@@ -1,16 +1,41 @@
-system.time({
+library(Hmisc) # wtd.mean(), wtd.var(), wtd.quantile()
+
+time. = system.time({
   files = list.files('data')
-  table. = list()
-  length(table.) = length(files)
+  tables = list(); length(tables) = length(files)
   
-  for(i in 81) {
+  for(i in 1:length(files)) {
     file.i = paste0('data/', files[i])
     if(i <= 21) col.name = 'ArrDelay' else col.name = 'ARR_DELAY'
-    
     dat = read.csv(file.i, colClasses = 'character')
     table.i = table(as.integer(dat[,col.name]))
     counts = as.numeric(table.i)
     arr.delay = as.integer(attributes(table.i)$dimnames[[1]])
-    table.[[i]] = cbind(counts, arr.delay)
+    tables[[i]] = cbind(counts, arr.delay)
   }
+  
+  save(tables, file = 'tables.rda')
+  
+  table. = do.call(rbind, tables)
+  
+  counts = by(table.[,1], table.[,2], sum, simplify = FALSE)
+  counts = as.numeric(counts)
+  arr.delay = sort(unique(table.[,2]))
+  
+  mu = wtd.mean(arr.delay, counts)
+  std = sqrt(wtd.var(arr.delay, counts))
+  quantiles = wtd.quantile(arr.delay, counts)
+  med = quantiles[3]
 })
+
+results2 = list(time = time., 
+                results = c(mean = mu, median = med, sd = std), 
+                quantiles = quantiles, 
+                system = Sys.info(), 
+                session = sessionInfo(), 
+                ram = '256gb', 
+                cpu.mhz = 2899.863, 
+                num.processors = 32, 
+                cpu.cores = 8, 
+                os = 'SL 6.3')
+save(results2, file = 'results2.rda')
