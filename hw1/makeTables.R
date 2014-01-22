26d25772b6da1356d8242435ccc3b76af6a9871c
chcai <chcai@ucdavis.edu>
2014-01-21 20:01:04 -0800

    clean up

--- hw1/makeTables.R
+++
@@ -1,6 +0,0 @@
-make.tables.fun = function(dat, col.num) {
-  table.i = table(as.integer(dat[,col.num]))
-  counts = as.numeric(table.i)
-  arr.delay = as.integer(attributes(table.i)$dimnames[[1]])
-  tables[[i]] = cbind(counts, arr.delay)
-}
